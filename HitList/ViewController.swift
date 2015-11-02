1d783b0ab5d8fa503e4d034615b990ba811b5e27
Husein Kareem <huseinkareem@gmail.com>
2015-11-02 12:56:37 -0600

    initial commit, use core data

---
+++ HitList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Husein Kareem on 11/2/15.
+//  Copyright © 2015 Vokal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
